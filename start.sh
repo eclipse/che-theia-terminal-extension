2cec6cbb6d550aeee92df31d34376df4b591e77b
Oleksandr Andriienko <oandriie@redhat.com>
2018-04-23 18:06:32 +0300

    Remove script for local development.
    Signed-off-by: Oleksandr Andriienko <oandriie@redhat.com>

--- start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-export CHE_WORKSPACE_ID=workspacecs82k5zp6jyv86fs && export CHE_API_EXTERNAL=http://192.168.0.103:8080/api && cd browser-app && yarn run start --hostName=0.0.0.0 --port=4000
